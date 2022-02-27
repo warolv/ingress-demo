4580524f40e753f8d51cc643c7ec9b07e0869f71
warolv <warolv@gmail.com>
2022-02-27 18:24:23 +0200

    Add tf scripts

--- README.md
+++ README.md
@@ -1,9 +1,10 @@
 
 # Prerequisites and used components
 
-## EKS cluster provision
+## Kubernetes cluster provisioning
 
-First need to provisioned k8s cluster (EKS on AWS in my case), terraform scripts to spin up EKS cluster attached (terraform folder), also instructuions to how provision EKS using terraform you can find in my repo: https://github.com/warolv/eks-demo
+First need to provisioned k8s cluster (EKS on AWS in my case), terraform scripts to spin up EKS cluster attached [terraform folder](terraform)
+Also instructuions of how to provision EKS using terraform you can find in my repo: https://github.com/warolv/eks-demo
 
   * VPC created with range: 172.16.0.0/16 
 
@@ -27,9 +28,9 @@ cert-manager is a native Kubernetes certificate management controller. It can he
 It will ensure certificates are valid and up to date, and attempt to renew certificates at a configured time before expiry.
 It is loosely based upon the work of kube-lego and has borrowed some wisdom from other similar projects such as kube-cert-manager.
 
-### Using Ingress Nginx as ingress on EKS as Cert Manager to issue self signed certificates
+### Using Ingress Nginx as ingress and Cert Manager to issue self signed certificates
 
-I will use Ingress Nginx as k8s ingress controller (use Classic Load Balancer) and Cert Manager to issue 'self signed' certificates for this solution (using it to issue Let's Encrypt certs in production use cases)
+I will use Ingress Nginx as ingress controller and Cert Manager to issue 'self signed' certificates for this solution (also may be used in production use cases to issue Let's Encrypt certs.)
 
 You can read in my post how I am using those components to secure traffic to your application: https://talks.cloudify.co/secure-traffic-to-your-application-with-kubernetes-and-cert-manager-cc2b44d29beb
 
@@ -47,6 +48,7 @@ named
 # Solution
   
   In case you not deployed ingress-nginx and cert manager as part of EKS provision with terraform, you can do it manually with helm:
+  
 
 
   Let's deploy first service to 'a-ns' namespace
---
+++ terraform/main.tf
@@ -0,0 +1,122 @@
+provider "aws" {
+  region = "eu-west-2"
+}
+
+data "aws_eks_cluster" "cluster" {
+  name = module.eks.cluster_id
+}
+
+data "aws_eks_cluster_auth" "cluster" {
+  name = module.eks.cluster_id
+}
+
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.cluster.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+  token                  = data.aws_eks_cluster_auth.cluster.token
+  load_config_file       = false
+}
+
+data "aws_availability_zones" "available" {
+}
+
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "3.11.0"
+
+  name                 = "k8s-${var.cluster_name}-vpc"
+  cidr                 = "172.16.0.0/16"
+  azs                  = data.aws_availability_zones.available.names
+  private_subnets      = ["172.16.1.0/24", "172.16.2.0/24"]
+  public_subnets       = ["172.16.3.0/24", "172.16.4.0/24"]
+  enable_nat_gateway   = true
+  single_nat_gateway   = true
+  enable_dns_hostnames = true
+
+  public_subnet_tags = {
+    "kubernetes.io/cluster/${var.cluster_name}" = "shared"
+    "kubernetes.io/role/elb"                    = "1"
+  }
+
+  private_subnet_tags = {
+    "kubernetes.io/cluster/${var.cluster_name}" = "shared"
+    "kubernetes.io/role/internal-elb"           = "1"
+  }
+}
+
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "17.24.0"
+
+  cluster_name    = "${var.cluster_name}"
+  cluster_version = "1.21"
+  subnets         = module.vpc.private_subnets
+
+  vpc_id = module.vpc.vpc_id
+  write_kubeconfig   = true
+
+  worker_groups = [
+    {
+        name = "ng-medium"
+        instance_type = "t3.medium"
+        asg_desired_capacity = 1
+        tags = [{
+          key                 = "instance-type"
+          value               = "on-demand-medium"
+          propagate_at_launch = true
+        }, {
+          key                 = "os-type"
+          value               = "linux"
+          propagate_at_launch = true
+        }]
+    },
+  ]
+}
+
+provider "helm" {
+  kubernetes {
+    host                   = data.aws_eks_cluster.cluster.endpoint
+    cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+    token                  = data.aws_eks_cluster_auth.cluster.token
+  }
+}
+
+resource "helm_release" "ingress" {
+  name       = "nginx-ingress"
+  repository = "https://charts.bitnami.com/bitnami"
+  chart      = "nginx-ingress-controller"
+  version    = "9.0.9"
+
+  set {
+    name  = "autoDiscoverAwsRegion"
+    value = "true"
+  }
+  set {
+    name  = "autoDiscoverAwsVpcID"
+    value = "true"
+  }
+  set {
+    name  = "clusterName"
+    value = var.cluster_name
+  }
+}
+
+resource "helm_release" "cert-manager" {
+  name       = "cert-manager"
+  repository = "https://charts.bitnami.com/bitnami"
+  chart      = "cert-manager"
+  version    = "0.1.26"
+
+  set {
+    name  = "autoDiscoverAwsRegion"
+    value = "true"
+  }
+  set {
+    name  = "autoDiscoverAwsVpcID"
+    value = "true"
+  }
+  set {
+    name  = "clusterName"
+    value = var.cluster_name
+  }
+}
